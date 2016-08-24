eeadc1827d26ed2a458417ece3deb6067f456abb
Mosesvb <vbmo2007@gmail.com>
2016-08-24 14:19:45 +0000

    added c# proj

---
+++ C#/PerfectNumber/PerfectNumber/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace PerfectNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(226, 262);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(101, 31);
+            this.btnFind.TabIndex = 0;
+            this.btnFind.Text = "&Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(76, 262);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(103, 31);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Given Number (N):";
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(157, 64);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(144, 20);
+            this.txtN.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 330);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnFind);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Perfect Number Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtN;
+    }
+}
+
---
+++ C#/PerfectNumber/PerfectNumber/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PerfectNumbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Function to calculate/find perfect number
+        private void FindPerfectNumber(int n)
+        {
+            //Variable to hold total number of N to find perfect number of it
+            int totalN = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i==0)
+                {
+                    totalN = totalN+i;
+                }
+            }
+            if (totalN == n)
+            {
+                MessageBox.Show("The Number (N) = " + n + " entered is a perfect number", "Number Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("The Number (N) = " + n + " entered is NOT a perfect number", "Number NOT Found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            //
+            FindPerfectNumber(int.Parse(txtN.Text.ToString()));
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
